5c3c02860a59c97f35464a990e5f859c387c6af4
Brahim EL Alami Chentoufi <bra.el.alami@gmail.com>
2023-04-25 07:57:42 -0700

    update

--- main.c
+++ main.c
@@ -7,7 +7,6 @@
  *
  * Return: Always 0
  */
-
 int main(void)
 {
 int len;
@@ -39,5 +38,7 @@ len = _printf("Percent:[%%]\n");
 len2 = printf("Percent:[%%]\n");
 _printf("Len:[%d]\n", len);
 printf("Len:[%d]\n", len2);
+_printf("Unknown:[%r]\n");
+printf("Unknown:[%r]\n");
 return (0);
 }
