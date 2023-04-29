1499a6b3180bb99325f1ce6caf37a767ef16e1a8
Brahim EL Alami Chentoufi <bra.el.alami@gmail.com>
2023-04-28 17:14:57 -0700

    update

--- main.h
+++ main.h
@@ -104,3 +104,12 @@ int width, int flags, char padd, char extra_c, int padd_start);
 int write_unsgnd(int is_negative, int ind,
 char buffer[],
 int flags, int width, int precision, int size);
+/****************** UTILS ******************/
+int is_printable(char);
+int append_hexa_code(char, char[], int);
+int is_digit(char);
+
+long int convert_size_number(long int num, int size);
+long int convert_size_unsgnd(unsigned long int num, int size);
+
+#endif /* MAIN_H */
